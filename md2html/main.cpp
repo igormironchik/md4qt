ee09153d58f7929afafa7e5d0e77adedabffd44f
Igor Mironchik <igor.mironchik@gmail.com>
2023-02-21 19:24:15 +0300

    Add function to convert MD::Document to HTML. (untested yet)

---
+++ md2html/main.cpp
@@ -0,0 +1,116 @@
+
+/*!
+	\file
+
+	\author Igor Mironchik (igor.mironchik at gmail dot com).
+
+	Copyright (c) 2023 Igor Mironchik
+
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following
+	conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+// args-parser include.
+#include <args-parser/all.hpp>
+
+// md4qt include.
+#define MD4QT_QT_SUPPORT
+#include <md4qt/traits.hpp>
+#include <md4qt/parser.hpp>
+#include <md4qt/html.hpp>
+
+// Qt include.
+#include <QFile>
+#include <QFileInfo>
+
+
+using namespace Args;
+using namespace MD;
+
+int main( int argc, char ** argv )
+{
+	try {
+		CmdLine cmd( argc, argv );
+
+		cmd.addArgWithFlagAndName( 'm', "markdown", true, true, "Markdown file to convert to HTML." )
+			.addArgWithFlagAndName( 'o', "html", true, true, "Output HTML file name." )
+			.addArgWithFlagAndName( 'r', "recursive", false, false, "Read all linked Markdown files?" )
+			.addHelp( true, argv[ 0 ], "Converter of Markdown to HTML." );
+
+		cmd.parse();
+
+		const auto markdownFileName = cmd.value( "-m" );
+		const auto htmlFileName = cmd.value( "-o" );
+		const auto recursive = cmd.isDefined( "-r" );
+
+		QFileInfo mdFileInfo( markdownFileName );
+
+		if( mdFileInfo.exists() )
+		{
+			if( mdFileInfo.suffix() == "md" || mdFileInfo.suffix() == "markdown" )
+			{
+				Parser< QStringTrait > parser;
+
+				const auto doc = parser.parse( markdownFileName, recursive );
+
+				QFile html( htmlFileName );
+
+				if( html.open( QIODevice::WriteOnly ) )
+				{
+					const auto content = toHtml( doc );
+
+					html.write( content.toUtf8() );
+
+					html.close();
+				}
+				else
+				{
+					outStream() << "Unable to write output HTML file.\n";
+
+					return 1;
+				}
+			}
+			else
+			{
+				outStream() << "Wrong file suffix of Markdown file.\n";
+
+				return 1;
+			}
+		}
+		else
+		{
+			outStream() << "Input Markdown file is not exist.\n";
+
+			return 1;
+		}
+	}
+	catch( const HelpHasBeenPrintedException & )
+	{
+	}
+	catch( const BaseException & x )
+	{
+		outStream() << x.desc() << "\n";
+
+		return 1;
+	}
+
+  return 0;
+}
---
+++ md4qt/html.hpp
@@ -0,0 +1,789 @@
+
+/*!
+	\file
+
+	\author Igor Mironchik (igor.mironchik at gmail dot com).
+
+	Copyright (c) 2023 Igor Mironchik
+
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following
+	conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#ifndef MD4QT_MD_HTML_HPP_INCLUDED
+#define MD4QT_MD_HTML_HPP_INCLUDED
+
+// md4qt include.
+#include "doc.hpp"
+
+// C++ include.
+#include <string>
+
+
+namespace MD {
+
+namespace details {
+
+template< class Trait >
+typename Trait::String
+headingIdToHtml( Heading< Trait > * h )
+{
+	typename Trait::String html;
+
+	if( h->isLabeled() )
+	{
+		html.push_back( " id=\"" );
+		auto label = h->label();
+		if( label.startsWith( typename Trait::Char( '#' ) ) )
+			label.remove( 0, 1 );
+		html.push_back( label );
+		html.push_back( "\"" );
+	}
+
+	return html;
+}
+
+template< class Trait >
+typename Trait::String
+openTextStyleToHtml( int s )
+{
+	typename Trait::String html;
+
+	if( s & TextOption::BoldText )
+		html.push_back( "<strong>" );
+
+	if( s & TextOption::ItalicText )
+		html.push_back( "<em>" );
+
+	if( s & TextOption::StrikethroughText )
+		html.push_back( "<del>" );
+
+	return html;
+}
+
+template< class Trait >
+typename Trait::String
+closeTextStyleToHtml( int s )
+{
+	typename Trait::String html;
+
+	if( s & TextOption::StrikethroughText )
+		html.push_back( "</del>" );
+
+	if( s & TextOption::ItalicText )
+		html.push_back( "</em>" );
+
+	if( s & TextOption::BoldText )
+		html.push_back( "</strong>" );
+
+	return html;
+}
+
+template< class Trait >
+typename Trait::String
+textToHtml( Text< Trait > * t )
+{
+	typename Trait::String html;
+
+	html.push_back( openTextStyleToHtml< Trait >( t->opts() ) );
+
+	if( t->isSpaceBefore() )
+		html.push_back( " " );
+
+	html.push_back( t->text() );
+
+	if( t->isSpaceAfter() )
+		html.push_back( " " );
+
+	html.push_back( closeTextStyleToHtml< Trait >( t->opts() ) );
+
+	return html;
+}
+
+template< class Trait >
+typename Trait::String
+linkTextToHtml( const typename Trait::String & text, int style )
+{
+	typename Trait::String html;
+
+	html.push_back( openTextStyleToHtml< Trait >( style ) );
+
+	html.push_back( text );
+
+	html.push_back( closeTextStyleToHtml< Trait >( style ) );
+
+	return html;
+}
+
+template< class Trait >
+typename Trait::String
+inlineCodeToHtml( Code< Trait > * c )
+{
+	typename Trait::String html;
+
+	html.push_back( "<code>" );
+
+	auto code = c->text();
+	code.replace( typename Trait::Char( '<' ), "&lt;" );
+	code.replace( typename Trait::Char( '>' ), "&gt;" );
+
+	html.push_back( code );
+
+	html.push_back( "</code>" );
+
+	return html;
+}
+
+template< class Trait >
+typename Trait::String
+imgToHtml( Image< Trait > * i )
+{
+	typename Trait::String html;
+
+	html.push_back( "<img src=\"" );
+	html.push_back( i->url() );
+	html.push_back( "\" alt=\"" );
+	html.push_back( i->text() );
+	html.push_back( "\" style=\"max-width:100%;\" />" );
+
+	return html;
+}
+
+template< class Trait >
+typename Trait::String
+linkToHtml( Link< Trait > * l, std::shared_ptr< Document< Trait > > doc )
+{
+	typename Trait::String html;
+
+	typename Trait::String url = l->url();
+
+	const auto lit = doc->labeledLinks().find( url );
+
+	if( lit != doc->labeledLinks().cend() )
+		url = lit->second->url();
+
+	if( typename Trait::Url( url ).isRelative() )
+		url = typename Trait::String( "#" ) + url;
+
+	html.push_back( "<a href=\"" );
+	html.push_back( url );
+	html.push_back( "\">" );
+
+	if( !l->img()->isEmpty() )
+		html.push_back( imgToHtml( l->img().get() ) );
+	else if( !l->text().isEmpty() )
+		html.push_back( linkTextToHtml< Trait >( l->text(), l->opts() ) );
+
+
+	html.push_back( "</a>" );
+
+	return html;
+}
+
+template< class Trait >
+typename Trait::String
+footnoteRefToHtml( FootnoteRef< Trait > * ref,
+	std::shared_ptr< Document< Trait > > doc,
+	typename Trait::template Vector< typename Trait::String > & fns )
+{
+	typename Trait::String html;
+
+	const auto fit = doc->footnotesMap().find( ref->id() );
+
+	if( fit != doc->footnotesMap().cend() )
+	{
+		html.push_back( "<a href=\"#" );
+		html.push_back( ref->id() );
+		html.push_back( "\">[" );
+		html.push_back( std::to_string( fns.size() + 1 ).c_str() );
+		html.push_back( "]</a>" );
+
+		fns.push_back( ref->id() );
+	}
+
+	return html;
+}
+
+template< class Trait >
+typename Trait::String
+paragraphToHtml( Paragraph< Trait > * p, bool wrap,
+	std::shared_ptr< Document< Trait > > doc,
+	typename Trait::template Vector< typename Trait::String > & fns )
+{
+	typename Trait::String html;
+
+	if( wrap )
+		html.push_back( "<p>" );
+
+	for( auto it = p->items().begin(), last = p->items().end(); it != last; ++it )
+	{
+		switch( (*it)->type() )
+		{
+			case ItemType::Text :
+				html.push_back( textToHtml( static_cast< Text< Trait >* > ( it->get() ) ) );
+				break;
+
+			case ItemType::Code :
+				html.push_back( inlineCodeToHtml( static_cast< Code< Trait >* > ( it->get() ) ) );
+				break;
+
+			case ItemType::Link :
+				html.push_back( linkToHtml( static_cast< Link< Trait >* > ( it->get() ), doc ) );
+				break;
+
+			case ItemType::Image :
+				html.push_back( imgToHtml( static_cast< Image< Trait >* > ( it->get() ) ) );
+				break;
+
+			case ItemType::Math :
+				break;
+
+			case ItemType::LineBreak :
+				html.push_back( "<br />\n" );
+				break;
+
+			case ItemType::FootnoteRef :
+				html.push_back( footnoteRefToHtml(
+					static_cast< FootnoteRef< Trait >* > ( it->get() ), doc, fns ) );
+				break;
+
+			default :
+				break;
+		}
+	}
+
+	if( wrap )
+		html.push_back( "</p>" );
+
+	return html;
+}
+
+template< class Trait >
+typename Trait::String
+headingToHtml( Heading< Trait > * h, const typename Trait::String & ht,
+	std::shared_ptr< Document< Trait > > doc,
+	typename Trait::template Vector< typename Trait::String > & fns )
+{
+	typename Trait::String html;
+
+	html.push_back( "<" );
+	html.push_back( ht );
+	html.push_back( headingIdToHtml( h ) );
+	html.push_back( ">" );
+
+	if( h->text().get() )
+		html.push_back( paragraphToHtml( h->text().get(), false, doc, fns ) );
+
+	html.push_back( "</" );
+	html.push_back( ht );
+	html.push_back( ">" );
+
+	return html;
+}
+
+template< class Trait >
+typename Trait::String
+headingToHtml( Heading< Trait > * h, std::shared_ptr< Document< Trait > > doc,
+	typename Trait::template Vector< typename Trait::String > & fns )
+{
+	typename Trait::String html;
+
+	html.push_back( "\n" );
+
+	switch( h->level() )
+	{
+		case 1 :
+			html.push_back( headingToHtml( h, "h1", doc, fns ) );
+			break;
+
+		case 2 :
+			html.push_back( headingToHtml( h, "h2", doc, fns ) );
+			break;
+
+		case 3 :
+			html.push_back( headingToHtml( h, "h3", doc, fns ) );
+			break;
+
+		case 4 :
+			html.push_back( headingToHtml( h, "h4", doc, fns ) );
+			break;
+
+		case 5 :
+			html.push_back( headingToHtml( h, "h5", doc, fns ) );
+			break;
+
+		case 6 :
+			html.push_back( headingToHtml( h, "h6", doc, fns ) );
+			break;
+
+		default :
+			break;
+	}
+
+	html.push_back( "\n" );
+
+	return html;
+}
+
+template< class Trait >
+typename Trait::String
+codeToHtml( Code< Trait > * c )
+{
+	typename Trait::String html;
+
+	html.push_back( "\n" );
+	html.push_back( "<pre><code" );
+
+	if( !c->syntax().isEmpty() )
+	{
+		html.push_back( " class=\"language-" );
+		html.push_back( c->syntax() );
+		html.push_back( "\"" );
+	}
+
+	html.push_back( ">" );
+	auto code = c->text();
+	code.replace( typename Trait::Char( '<' ), "&lt;" );
+	code.replace( typename Trait::Char( '>' ), "&gt;" );
+	html.push_back( code );
+	html.push_back( "</code></pre>" );
+	html.push_back( "\n" );
+
+	return html;
+}
+
+template< class Trait >
+typename Trait::String
+blockquoteToHtml( Blockquote< Trait > * b, std::shared_ptr< Document< Trait > > doc,
+	typename Trait::template Vector< typename Trait::String > & fns );
+
+template< class Trait >
+typename Trait::String
+listToHtml( List< Trait > * l, std::shared_ptr< Document< Trait > > doc,
+	typename Trait::template Vector< typename Trait::String > & fns );
+
+template< class Trait >
+typename Trait::String
+tableToHtml( Table< Trait > * t, std::shared_ptr< Document< Trait > > doc,
+	typename Trait::template Vector< typename Trait::String > & fns );
+
+template< class Trait >
+typename Trait::String
+listItemToHtml( ListItem< Trait > * i, std::shared_ptr< Document< Trait > > doc,
+	typename Trait::template Vector< typename Trait::String > & fns )
+{
+	typename Trait::String html;
+
+	html.push_back( "<li>\n" );
+
+	for( auto it = i->items().cbegin(), last = i->items().cend(); it != last; ++it )
+	{
+		switch( (*it)->type() )
+		{
+			case ItemType::Heading :
+				html.push_back( headingToHtml(
+					static_cast< Heading< Trait >* > ( it->get() ), doc, fns ) );
+				break;
+
+			case ItemType::Paragraph :
+				html.push_back( paragraphToHtml(
+					static_cast< Paragraph< Trait >* > ( it->get() ), false, doc, fns ) );
+				break;
+
+			case ItemType::Code :
+				html.push_back( codeToHtml( static_cast< Code< Trait >* > ( it->get() ) ) );
+				break;
+
+			case ItemType::Blockquote :
+				html.push_back( blockquoteToHtml(
+					static_cast< Blockquote< Trait >* > ( it->get() ), doc, fns ) );
+				break;
+
+			case ItemType::List :
+				html.push_back( listToHtml(
+					static_cast< List< Trait >* > ( it->get() ), doc, fns ) );
+				break;
+
+			case ItemType::Table :
+				html.push_back( tableToHtml(
+					static_cast< Table< Trait >* > ( it->get() ), doc, fns ) );
+				break;
+
+			case ItemType::RawHtml :
+				html.push_back( static_cast< RawHtml< Trait>* > ( it->get() )->text() );
+				break;
+
+			default :
+				break;
+		}
+	}
+
+	html.push_back( "</li>\n" );
+
+	return html;
+}
+
+template< class Trait >
+typename Trait::String
+listToHtml( List< Trait > * l, std::shared_ptr< Document< Trait > > doc,
+	typename Trait::template Vector< typename Trait::String > & fns )
+{
+	typename Trait::String html;
+
+	html.push_back( "\n" );
+
+	typename ListItem< Trait >::ListType type = ListItem< Trait >::Ordered;
+	bool first = true;
+
+	for( auto it = l->items().cbegin(), last = l->items().cend(); it != last; ++it )
+	{
+		if( (*it)->type() == ItemType::ListItem )
+		{
+			auto * item = static_cast< ListItem< Trait >* > ( it->get() );
+
+			if( first )
+			{
+				type = item->listType();
+				first = false;
+
+				if( type == ListItem< Trait >::Ordered )
+					html.push_back( "<ol>\n" );
+				else
+					html.push_back( "<ul>\n" );
+			}
+			else
+			{
+				if( item->listType() != type )
+				{
+					if( type == ListItem< Trait >::Ordered )
+						html.push_back( "</ol><ul>\n" );
+					else
+						html.push_back( "</ul><ol>\n" );
+
+					type = item->listType();
+				}
+			}
+
+			html.push_back( listItemToHtml( item, doc, fns ) );
+		}
+	}
+
+	if( !first )
+	{
+		if( type == ListItem< Trait >::Ordered )
+			html.push_back( "</ol>\n" );
+		else
+			html.push_back( "</ul>\n" );
+	}
+
+	return html;
+}
+
+template< class Trait >
+typename Trait::String
+cellToHtml( TableCell< Trait > * l, std::shared_ptr< Document< Trait > > doc,
+	typename Trait::template Vector< typename Trait::String > & fns )
+{
+	typename Trait::String html;
+
+	for( auto it = l->items().cbegin(), last = l->items().cend(); it != last; ++it )
+	{
+		switch( (*it)->type() )
+		{
+			case ItemType::Text :
+				html.push_back( textToHtml( static_cast< Text< Trait >* > ( it->get() ) ) );
+				break;
+
+			case ItemType::Code :
+				html.push_back( inlineCodeToHtml( static_cast< Code< Trait >* > ( it->get() ) ) );
+				break;
+
+			case ItemType::Link :
+				html.push_back( linkToHtml(
+					static_cast< Link< Trait >* > ( it->get() ), doc ) );
+				break;
+
+			case ItemType::Image :
+				html.push_back( imgToHtml( static_cast< Image< Trait >* > ( it->get() ) ) );
+				break;
+
+			case ItemType::FootnoteRef :
+				html.push_back( footnoteRefToHtml(
+					static_cast< FootnoteRef< Trait >* > ( it->get() ), doc, fns ) );
+				break;
+
+			case ItemType::RawHtml :
+			{
+				auto * r = static_cast< RawHtml< Trait >* > ( it->get() );
+
+				html.push_back( r->text() );
+			}
+				break;
+
+			default :
+				break;
+		}
+	}
+
+	return html;
+}
+
+template< class Trait >
+typename Trait::String
+tableAlignmentToHtml( typename Table< Trait >::Alignment a )
+{
+	typename Trait::String html;
+
+	switch( a )
+	{
+		case Table< Trait >::AlignLeft :
+			html.push_back( " align=\"left\"" );
+			break;
+
+		case Table< Trait >::AlignCenter :
+			html.push_back( " align=\"center\"" );
+			break;
+
+		case Table< Trait >::AlignRight :
+			html.push_back( " align=\"right\"" );
+			break;
+
+		default :
+			break;
+	}
+
+	return html;
+}
+
+template< class Trait >
+typename Trait::String
+tableToHtml( Table< Trait > * t, std::shared_ptr< Document< Trait > > doc,
+	typename Trait::template Vector< typename Trait::String > & fns )
+{
+	typename Trait::String html;
+
+	html.push_back( "\n" );
+
+	if( !t->isEmpty() )
+	{
+		html.push_back( "<table style=\"width:100%;border:1px solid black;border-collapse:collapse;max-width:100%;\">"
+						"<thead><tr>\n" );
+
+		int i = 0;
+
+		for( auto th = (*t->rows().cbegin())->cells().cbegin(),
+			last = (*t->rows().cbegin())->cells().cend(); th != last; ++th )
+		{
+			html.push_back( "<th" );
+			html.push_back( tableAlignmentToHtml< Trait >( t->columnAlignment( i ) ) );
+			html.push_back( " style=\"border:1px solid black;border-collapse:collapse;max-width:100%;\">\n" );
+			html.push_back( cellToHtml( th->get(), doc, fns ) );
+			html.push_back( "\n</th>\n" );
+
+			++i;
+		}
+
+		html.push_back( "</thead><tbody>\n" );
+
+		for( auto r = std::next( t->rows().cbegin() ), rlast = t->rows().cend(); r != rlast; ++r )
+		{
+			html.push_back( "<tr>\n" );
+
+			i = 0;
+
+			for( auto c = (*r)->cells().cbegin(), clast = (*r)->cells().cend(); c != clast; ++c )
+			{
+				html.push_back( "\n<td" );
+				html.push_back( tableAlignmentToHtml< Trait >( t->columnAlignment( i ) ) );
+				html.push_back( " style=\"border:1px solid black;border-collapse:collapse;max-width:100%;\">\n" );
+				html.push_back( cellToHtml( c->get(), doc, fns ) );
+				html.push_back( "\n</td>\n" );
+
+				++i;
+			}
+
+			html.push_back( "\n</tr>\n" );
+		}
+
+		html.push_back( "</tbody></table>" );
+	}
+
+	html.push_back( "\n" );
+
+	return html;
+}
+
+template< class Trait >
+typename Trait::String
+blockquoteToHtml( Blockquote< Trait > * b, std::shared_ptr< Document< Trait > > doc,
+	typename Trait::template Vector< typename Trait::String > & fns )
+{
+	typename Trait::String html;
+
+	html.push_back( "\n<blockquote>" );
+
+	for( auto it = b->items().cbegin(), last = b->items().cend(); it != last; ++it )
+	{
+		switch( (*it)->type() )
+		{
+			case ItemType::Heading :
+				html.push_back( headingToHtml(
+					static_cast< Heading< Trait >* > ( it->get() ), doc, fns ) );
+				break;
+
+			case ItemType::Paragraph :
+				html.push_back( paragraphToHtml(
+					static_cast< Paragraph< Trait >* > ( it->get() ), true, doc, fns ) );
+				break;
+
+			case ItemType::Code :
+				html.push_back( codeToHtml(
+					static_cast< Code< Trait >* > ( it->get() ) ) );
+				break;
+
+			case ItemType::Blockquote :
+				html.push_back( blockquoteToHtml(
+					static_cast< Blockquote< Trait >* > ( it->get() ), doc, fns ) );
+				break;
+
+			case ItemType::List :
+				html.push_back( listToHtml(
+					static_cast< List< Trait >* > ( it->get() ), doc, fns ) );
+				break;
+
+			case ItemType::Table :
+				html.push_back( tableToHtml(
+					static_cast< Table< Trait >* > ( it->get() ), doc, fns ) );
+				break;
+
+			case ItemType::HorizontalLine :
+				html.push_back( "<hr />" );
+				break;
+
+			case ItemType::RawHtml :
+			{
+				auto * r = static_cast< RawHtml< Trait >* > ( it->get() );
+
+				html.push_back( r->text() );
+			}
+				break;
+
+			default :
+				break;
+		}
+	}
+
+	html.push_back( "</blockquote>\n" );
+
+	return html;
+}
+
+template< class Trait >
+typename Trait::String
+anchorToHtml( Anchor< Trait > * a )
+{
+	typename Trait::String html;
+
+	html.push_back( "\n<div id=\"" );
+	html.push_back( a->label() );
+	html.push_back( "\"></div>\n" );
+
+	return html;
+}
+
+} /* namespace details */
+
+template< class Trait >
+typename Trait::String
+toHtml( std::shared_ptr< Document< Trait > > doc )
+{
+	typename Trait::String html;
+
+	typename Trait::template Vector< typename Trait::String > fns;
+
+	html.push_back( "<!DOCTYPE html>\n<html><head></head><body>" );
+
+	for( auto it = doc->items().cbegin(), last = doc->items().cend(); it != last; ++it )
+	{
+		switch( (*it)->type() )
+		{
+			case ItemType::Heading :
+				html.push_back( details::headingToHtml(
+					static_cast< Heading< Trait >* > ( it->get() ), doc, fns ) );
+				break;
+
+			case ItemType::Paragraph :
+				html.push_back( details::paragraphToHtml(
+					static_cast< Paragraph< Trait >* > ( it->get() ), true, doc, fns ) );
+				break;
+
+			case ItemType::Code :
+				html.push_back( details::codeToHtml(
+					static_cast< Code< Trait >* > ( it->get() ) ) );
+				break;
+
+			case ItemType::Blockquote :
+				html.push_back( details::blockquoteToHtml(
+					static_cast< Blockquote< Trait >* > ( it->get() ), doc, fns ) );
+				break;
+
+			case ItemType::List :
+				html.push_back( details::listToHtml(
+					static_cast< List< Trait >* > ( it->get() ), doc, fns ) );
+				break;
+
+			case ItemType::Table :
+				html.push_back( details::tableToHtml(
+					static_cast< Table< Trait >* > ( it->get() ), doc, fns ) );
+				break;
+
+			case ItemType::Anchor :
+				html.push_back( details::anchorToHtml(
+					static_cast< Anchor< Trait >* > ( it->get() ) ) );
+				break;
+
+			case ItemType::RawHtml :
+			{
+				auto * r = static_cast< RawHtml< Trait >* > ( it->get() );
+
+				html.push_back( "\n" );
+				html.push_back( r->text() );
+				html.push_back( "\n" );
+			}
+				break;
+
+			case ItemType::HorizontalLine :
+				html.push_back( "<hr />" );
+				break;
+
+			default :
+				break;
+		}
+	}
+
+	html.push_back( "</body></html>\n" );
+
+	return html;
+}
+
+} /* namespace MD */
+
+#endif // MD4QT_MD_HTML_HPP_INCLUDED
