7dcdca5c3a8892f576f7ba68a4fa89c14cbfcb51
Igor Mironchik <igor.mironchik@gmail.com>
2023-03-07 09:28:12 +0300

    Add test.

--- tests/auto/test_parser/main.cpp
+++ tests/auto/test_parser/main.cpp
@@ -5238,6 +5238,11 @@ TEST_CASE( "072" )
 	REQUIRE( t->endLine() == 5 );
 }
 
+/*
+<pre>abc</pre>
+Text
+
+*/
 TEST_CASE( "073" )
 {
 	MD::Parser< TRAIT > parser;
@@ -5250,12 +5255,24 @@ TEST_CASE( "073" )
 	REQUIRE( doc->items().at( 1 )->type() == MD::ItemType::RawHtml );
 	auto h = static_cast< MD::RawHtml< TRAIT >* > ( doc->items().at( 1 ).get() );
 	REQUIRE( h->text() == u8"<pre>abc</pre>" );
+	REQUIRE( h->startColumn() == 0 );
+	REQUIRE( h->startLine() == 0 );
+	REQUIRE( h->endColumn() == 13 );
+	REQUIRE( h->endLine() == 0 );
 
 	REQUIRE( doc->items().at( 2 )->type() == MD::ItemType::Paragraph );
 	auto p = static_cast< MD::Paragraph< TRAIT >* > ( doc->items().at( 2 ).get() );
+	REQUIRE( p->startColumn() == 0 );
+	REQUIRE( p->startLine() == 1 );
+	REQUIRE( p->endColumn() == 3 );
+	REQUIRE( p->endLine() == 1 );
 	REQUIRE( p->items().size() == 1 );
 	REQUIRE( p->items().at( 0 )->type() == MD::ItemType::Text );
 	auto t = static_cast< MD::Text< TRAIT >* > ( p->items().at( 0 ).get() );
+	REQUIRE( t->startColumn() == 0 );
+	REQUIRE( t->startLine() == 1 );
+	REQUIRE( t->endColumn() == 3 );
+	REQUIRE( t->endLine() == 1 );
 	REQUIRE( t->opts() == MD::TextWithoutFormat );
 	REQUIRE( t->text() == u8"Text" );
 }
