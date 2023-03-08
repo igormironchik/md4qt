ffe9c4058abf85915d7c09225ca8a8b32f7b2564
Igor Mironchik <igor.mironchik@gmail.com>
2023-03-08 10:33:18 +0300

    Add test.

--- tests/auto/test_parser/main.cpp
+++ tests/auto/test_parser/main.cpp
@@ -5646,6 +5646,12 @@ TEST_CASE( "082" )
 	}
 }
 
+/*
+Text
+<!-- Comment -->
+Text
+
+*/
 TEST_CASE( "083" )
 {
 	MD::Parser< TRAIT > parser;
@@ -5658,25 +5664,45 @@ TEST_CASE( "083" )
 	{
 		REQUIRE( doc->items().at( 1 )->type() == MD::ItemType::Paragraph );
 		auto p = static_cast< MD::Paragraph< TRAIT >* > ( doc->items().at( 1 ).get() );
+		REQUIRE( p->startColumn() == 0 );
+		REQUIRE( p->startLine() == 0 );
+		REQUIRE( p->endColumn() == 3 );
+		REQUIRE( p->endLine() == 0 );
 		REQUIRE( p->items().size() == 1 );
 		REQUIRE( p->items().at( 0 )->type() == MD::ItemType::Text );
 		auto t = static_cast< MD::Text< TRAIT >* > ( p->items().at( 0 ).get() );
 		REQUIRE( t->opts() == MD::TextWithoutFormat );
 		REQUIRE( t->text() == u8"Text" );
+		REQUIRE( t->startColumn() == 0 );
+		REQUIRE( t->startLine() == 0 );
+		REQUIRE( t->endColumn() == 3 );
+		REQUIRE( t->endLine() == 0 );
 	}
 
 	REQUIRE( doc->items().at( 2 )->type() == MD::ItemType::RawHtml );
 	auto h = static_cast< MD::RawHtml< TRAIT >* > ( doc->items().at( 2 ).get() );
 	REQUIRE( h->text() == u8"<!-- Comment -->" );
+	REQUIRE( h->startColumn() == 0 );
+	REQUIRE( h->startLine() == 1 );
+	REQUIRE( h->endColumn() == 15 );
+	REQUIRE( h->endLine() == 1 );
 
 	{
 		REQUIRE( doc->items().at( 3 )->type() == MD::ItemType::Paragraph );
 		auto p = static_cast< MD::Paragraph< TRAIT >* > ( doc->items().at( 3 ).get() );
+		REQUIRE( p->startColumn() == 0 );
+		REQUIRE( p->startLine() == 2 );
+		REQUIRE( p->endColumn() == 3 );
+		REQUIRE( p->endLine() == 2 );
 		REQUIRE( p->items().size() == 1 );
 		REQUIRE( p->items().at( 0 )->type() == MD::ItemType::Text );
 		auto t = static_cast< MD::Text< TRAIT >* > ( p->items().at( 0 ).get() );
 		REQUIRE( t->opts() == MD::TextWithoutFormat );
 		REQUIRE( t->text() == u8"Text" );
+		REQUIRE( t->startColumn() == 0 );
+		REQUIRE( t->startLine() == 2 );
+		REQUIRE( t->endColumn() == 3 );
+		REQUIRE( t->endLine() == 2 );
 	}
 }
 
