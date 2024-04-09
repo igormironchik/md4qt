91280a34baf7a0e9eb282b889d0a1771956dbda6
Igor Mironchik <igor.mironchik@gmail.com>
2024-04-09 08:42:05 +0300

    Make it possible to build without Conan.

---
+++ cmake/Finduriparser.cmake
@@ -0,0 +1,20 @@
+
+if( NOT URIPARSER_INCLUDE_DIR )
+	find_path( URIPARSER_INCLUDE_DIR NAMES Uri.h PATH_SUFFIXES uriparser )
+	set( URIPARSER_INCLUDE_DIR ${URIPARSER_INCLUDE_DIR}/uriparser CACHE PATH "uriparser includes" )
+endif()
+
+find_library( URIPARSER_LIBRARY NAMES uriparser )
+
+if( URIPARSER_INCLUDE_DIR AND URIPARSER_LIBRARY )
+	set( URIPARSER_FOUND TRUE )
+	set( UriParser_FOUND TRUE )
+	
+	if( NOT TARGET uriparser::uriparser )
+		add_library( uriparser::uriparser UNKNOWN IMPORTED )
+
+		target_include_directories( uriparser::uriparser INTERFACE ${URIPARSER_INCLUDE_DIR} )
+		set_target_properties( uriparser::uriparser PROPERTIES
+			IMPORTED_LOCATION ${URIPARSER_LIBRARY} )
+	endif()
+endif ()
