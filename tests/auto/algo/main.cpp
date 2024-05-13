04d71f9f55dd533c7e6697bd7db084ed01ab0ceb
Igor Mironchik <igor.mironchik@gmail.com>
2024-05-13 14:33:43 +0300

    Add tests.

--- tests/auto/algo/main.cpp
+++ tests/auto/algo/main.cpp
@@ -78,6 +78,23 @@ TEST_CASE( "003" )
 }
 
 /*
+Line 1...<space><space>
+Line 2...
+Line 3...
+
+*/
+TEST_CASE( "005" )
+{
+	MD::Parser< TRAIT > parser;
+
+	auto doc = parser.parse( "tests/parser/data/005.md" );
+	
+	MD::forEach< TRAIT >( { MD::ItemType::LineBreak }, doc, onItem );
+	REQUIRE( g_count == 1 );
+	g_count = 0;
+}
+
+/*
 `code`
 
 */
@@ -151,6 +168,23 @@ TEST_CASE( "017" )
 }
 
 /*
+	if( a > b )
+      do_something();
+    else
+      dont_do_anything();
+*/
+TEST_CASE( "021" )
+{
+	MD::Parser< TRAIT > parser;
+
+	auto doc = parser.parse( "tests/parser/data/021.md" );
+	
+	MD::forEach< TRAIT >( { MD::ItemType::Code }, doc, onItem );
+	REQUIRE( g_count == 1 );
+	g_count = 0;
+}
+
+/*
 * Item 1
 
     Paragraph in list
@@ -198,5 +232,194 @@ TEST_CASE( "029" )
 	
 	MD::forEach< TRAIT >( { MD::ItemType::Paragraph }, doc, onItem , 3 );
 	REQUIRE( g_count == 7 );
+	g_count = 0;
+}
+
+/*
+Text ![Image 1](a.jpg) continue ![ Image 2 ](b.png) and ![ Image 3]( http://www.where.com/c.jpeg "description" )
+
+*/
+TEST_CASE( "030" )
+{
+	MD::Parser< TRAIT > parser;	
+	auto doc = parser.parse( "tests/parser/data/030.md" );
+	
+	MD::forEach< TRAIT >( { MD::ItemType::Image }, doc, onItem );
+	REQUIRE( g_count == 3 );
+	g_count = 0;
+}
+
+/*
+[link 0][wrong-label] [link 1](a.md) [![image 1](a.png)](b.md) [link 3][label] [^ref]
+
+[label]: http://www.where.com/a.md (caption)
+
+[^ref] text
+
+[1]: a.md 'title'
+
+[link 4](#label)
+
+*/
+TEST_CASE( "031" )
+{
+	MD::Parser< TRAIT > parser;
+
+	auto doc = parser.parse( "tests/parser/data/031.md" );
+	
+	MD::forEach< TRAIT >( { MD::ItemType::Link }, doc, onItem );
+	REQUIRE( g_count == 4 );
+	g_count = 0;
+	
+	MD::forEach< TRAIT >( { MD::ItemType::FootnoteRef }, doc, onItem );
+	REQUIRE( g_count == 2 );
+	g_count = 0;
+	
+	MD::forEach< TRAIT >( { MD::ItemType::Image }, doc, onItem );
+	REQUIRE( g_count == 1 );
+	g_count = 0;
+}
+
+/*
+Heading 1
+=========
+Paragraph 1
+
+Heading 2
+---------
+Paragraph 2
+
+  # Heading 1
+
+Paragraph 1
+
+## Heading 2
+
+Paragraph 2
+
+Heading 1
+=========
+
+Paragraph 1
+
+Heading 2
+---------
+
+Paragraph 2
+
+### Heading 3 {#heading-3}
+
+*/
+TEST_CASE( "046" )
+{
+	MD::Parser< TRAIT > parser;
+
+	auto doc = parser.parse( "tests/parser/data/046.md" );
+	
+	MD::forEach< TRAIT >( { MD::ItemType::Heading }, doc, onItem );
+	REQUIRE( g_count == 7 );
+	g_count = 0;
+}
+
+/*
+When $a \ne 0$, there are two solutions to $(ax^2 + bx + c = 0)$ and they are
+$$ x = {-b \pm \sqrt{b^2-4ac} \over 2a} $$
+
+*/
+TEST_CASE( "065" )
+{
+	MD::Parser< TRAIT > parser;
+
+	auto doc = parser.parse( "tests/parser/data/065.md" );
+	
+	MD::forEach< TRAIT >( { MD::ItemType::Math }, doc, onItem );
+	REQUIRE( g_count == 3 );
+	g_count = 0;
+}
+
+/*
+| table |
+| ----- |
+| <img src="img/img.png"> |
+
+*/
+TEST_CASE( "168" )
+{
+	MD::Parser< TRAIT > parser;
+
+	auto doc = parser.parse( "tests/parser/data/168.md" );
+	
+	MD::forEach< TRAIT >( { MD::ItemType::Table }, doc, onItem );
+	REQUIRE( g_count == 1 );
+	g_count = 0;
+	
+	MD::forEach< TRAIT >( { MD::ItemType::RawHtml }, doc, onItem );
+	REQUIRE( g_count == 1 );
+	g_count = 0;
+}
+
+/*
+Text
+***
+| table |
+| --- |
+| data |
+
+*/
+TEST_CASE( "174" )
+{
+	MD::Parser< TRAIT > parser;
+
+	auto doc = parser.parse( "tests/parser/data/174.md" );
+	
+	MD::forEach< TRAIT >( { MD::ItemType::HorizontalLine }, doc, onItem );
+	REQUIRE( g_count == 1 );
+	g_count = 0;
+	
+	MD::forEach< TRAIT >( { MD::ItemType::Table }, doc, onItem );
+	REQUIRE( g_count == 1 );
+	g_count = 0;
+	
+	MD::forEach< TRAIT >( { MD::ItemType::Paragraph }, doc, onItem );
+	REQUIRE( g_count == 1 );
+	g_count = 0;
+	
+	MD::forEach< TRAIT >( { MD::ItemType::Text }, doc, onItem );
+	REQUIRE( g_count == 3 );
+	g_count = 0;
+}
+
+/*
+Text[^1]:
+
+[^1]: Footnote.
+
+```
+code
+```
+
+Text.
+
+*/
+TEST_CASE( "191" )
+{
+	MD::Parser< TRAIT > parser;
+
+	auto doc = parser.parse( "tests/parser/data/191.md" );
+	
+	MD::forEach< TRAIT >( { MD::ItemType::Footnote }, doc, onItem );
+	REQUIRE( g_count == 1 );
+	g_count = 0;
+	
+	MD::forEach< TRAIT >( { MD::ItemType::FootnoteRef }, doc, onItem );
+	REQUIRE( g_count == 1 );
+	g_count = 0;
+	
+	MD::forEach< TRAIT >( { MD::ItemType::Code }, doc, onItem );
+	REQUIRE( g_count == 1 );
+	g_count = 0;
+	
+	MD::forEach< TRAIT >( { MD::ItemType::Paragraph }, doc, onItem );
+	REQUIRE( g_count == 3 );
 	g_count = 0;
 }
