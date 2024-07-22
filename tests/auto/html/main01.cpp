003c2b6e35de9810c6c639ebe3eb4def1065a10e
Igor Mironchik <igor.mironchik@gmail.com>
2024-07-22 07:34:54 +0300

    Add test.

--- tests/auto/html/main01.cpp
+++ tests/auto/html/main01.cpp
@@ -20,3 +20,14 @@ TEST_CASE( "001" )
 	REQUIRE( html == u8"<p><em><em><a href=\"www.google.com\"> google </a>"
 		"</em> text </em></p>" );
 }
+
+/*
+**text* text*
+
+*/
+TEST_CASE( "002" )
+{
+	MD::Parser< TRAIT > p;
+	auto html = MD::toHtml( p.parse( "tests/html/data/002.md" ), false, {}, false );
+	REQUIRE( html == u8"<p><em><em> text text </em></em></p>" );
+}
